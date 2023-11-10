bcfcd3f87e9392a4254a2e8b0696dcf282b9d11a
mhiga701 <112960364+mhiga701@users.noreply.github.com>
2023-11-10 16:22:42 -0500

    more navbar edits'

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
 import Page from '../app/main';
 import Profile from '../app/profile';
 import { Text, View } from 'react-native';
@@ -8,13 +10,25 @@ import {styles} from './styles';
 
 const Tab = createBottomTabNavigator();
 
-export default function Nav() {
+function BarTabs() {
   return (
-    <Tab.Navigator style={styles.navContainer}>
-      <Tab.Screen name="Home" component={Page} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Reporting" component={RPage} />
-      {/* Add more Tab.Screen components for additional tabs */}
+    <Tab.Navigator style={styles.navContainer} initialRouteName='Home'>
+      <Tab.Screen name="Home" component={Page}/>
+      <Tab.Screen name="Profile" component={Profile}/>
+      <Tab.Screen name="Reporting" component={RPage}/>
     </Tab.Navigator>
   );
+}
+
+const Stack = createStackNavigator();
+
+export default function Nav() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={BarTabs} />
+        {/* Add more Stack.Screen components for additional pages */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
