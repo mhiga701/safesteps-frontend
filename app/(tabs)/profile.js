9e75300099e794420727a570f622d99dec94f356
rithvik-doshi <rithvik@bu.edu>
2023-12-03 12:42:30 -0500

    WORKING BLUETOOTH EEEE

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -1,51 +1,50 @@
 import { React } from "react";
 import { Text, ScrollView, View } from "react-native";
 import { styles } from "../../components/styles";
-import { BleManager } from "react-native-ble-plx";
 import FeedbackForm from "../../components/FeedbackForm";
 import VisAlert from "../../components/VisAlert";
 import AudioAlert from "../../components/AudioAlert";
 import AlertHeader from "../../assets/AlertTypes3.svg";
 
 export default function Profile() {
-  const manager = new BleManager();
-  // const [sound, setSound] = useState(new Audio.Sound());
-
   return (
     <>
-    <View style={styles.headerContainer}>
-      <AlertHeader style={styles.alertHeader}/>
-    </View>
+      <View style={styles.headerContainer}>
+        <AlertHeader style={styles.alertHeader} />
+      </View>
 
-        <Text style={{color: '#f2f2f2', fontSize: 24,
-    fontWeight: "700",
-    marginTop: 100,
-    marginLeft: 20,
-    fontFamily: "Montserrat-Bold",
-    position: "absolute",}}>Alert Types</Text>
-    <View style={styles.profileContainer}>
-
-   
-      <ScrollView
-        
-        automaticallyAdjustKeyboardInsets={true}
-        automaticallyAdjustContentInsets={true}
-        // bounces={false}
-        // contentContainerStyle={{
-        //   flexGrow: 1,
-        // }}
+      <Text
+        style={{
+          color: "#f2f2f2",
+          fontSize: 24,
+          fontWeight: "700",
+          marginTop: 100,
+          marginLeft: 20,
+          fontFamily: "Montserrat-Bold",
+          position: "absolute",
+        }}
       >
-        
-        <VisAlert />
+        Alert Types
+      </Text>
+      <View style={styles.profileContainer}>
+        <ScrollView
+          automaticallyAdjustKeyboardInsets={true}
+          automaticallyAdjustContentInsets={true}
+          // bounces={false}
+          contentContainerStyle={{
+            flexGrow: 1,
+          }}
+        >
+          <VisAlert />
 
-        <Text style={styles.subheadingText}>AUDIO ALERT</Text>
+          <Text style={styles.subheadingText}>AUDIO ALERT</Text>
 
-        <AudioAlert />
+          <AudioAlert />
 
-        <Text style={styles.subheadingText}>Feedback Form</Text>
+          <Text style={styles.subheadingText}>Feedback Form</Text>
 
-        <FeedbackForm />
-      </ScrollView>
+          <FeedbackForm />
+        </ScrollView>
       </View>
     </>
   );
