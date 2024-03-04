3171d03211abc1de0fa4a71cbbf86120d6444e04
mhiga701 <112960364+mhiga701@users.noreply.github.com>
2024-03-04 11:07:14 -0500

    refactoring onboarding sequence

--- app/_layout.js
+++ app/_layout.js
@@ -5,8 +5,8 @@ import { View, Text, TouchableOpacity, Modal } from "react-native";
 import { styles } from "../components/styles";
 import Splash1 from "../assets/Logo.svg";
 import Splash2 from "../assets/logo2.svg";
-import { RootSiblingParent } from "react-native-root-siblings";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+
 
 const router = useRouter();
 export const unstable_settings = {
@@ -15,65 +15,66 @@ export const unstable_settings = {
 
 
 
-function RootLayoutNav() {
+export default function RootLayoutNav() {
   return (
     <>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="onboarding" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </>
   );
 }
 
-export default function RootLayout() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-  const [splash, setSplash] = useState(true);
+// export default function RootLayout() {
+//   const [fontLoaded, setFontLoaded] = useState(false);
+//   const [splash, setSplash] = useState(true);
 
-  async function loadFont() {
-    await Font.loadAsync({
-      "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.otf"),
-      "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.otf"),
-      "Bitter-Regular": require("../assets/fonts/Bitter-Regular.otf"),
-      "Montserrat-SemiBold": require("../assets/fonts/Montserrat-SemiBold.otf"),
-    });
-    setFontLoaded(true);
-  }
-  loadFont();
+//   async function loadFont() {
+//     await Font.loadAsync({
+//       "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.otf"),
+//       "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.otf"),
+//       "Bitter-Regular": require("../assets/fonts/Bitter-Regular.otf"),
+//       "Montserrat-SemiBold": require("../assets/fonts/Montserrat-SemiBold.otf"),
+//     });
+//     setFontLoaded(true);
+//   }
+//   loadFont();
 
-  if (!fontLoaded) {
-    return <Splash1 />;
-  } else {
-    if (splash) {
-      return (
-        <Modal visible={splash} animationType="fade">
-          <View style={styles.modalContainer}>
-            <Splash2 style={styles.backgroundImage} />
-            <View style={styles.contentContainer}>
-              <TouchableOpacity
-                style={styles.buttonHomeScreen}
-                onPress={() => {
-                  setSplash(false);
-                }}
-              >
-                <Text style={styles.buttonText}>Get Started</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </Modal>
-      );
-    } else {
-      return (
-        <>
-          <GestureHandlerRootView style={{ flex: 1 }}>
-            <RootSiblingParent>
+//   if (!fontLoaded) {
+//     return <Splash1 />;
+//   } else {
+//     if (splash) {
+//       return (
+//         <Modal visible={splash} animationType="fade">
+//           <View style={styles.modalContainer}>
+//             <Splash2 style={styles.backgroundImage} />
+//             <View style={styles.contentContainer}>
+//               <TouchableOpacity
+//                 style={styles.buttonHomeScreen}
+//                 onPress={() => {
+//                   setSplash(false);
+//                 }}
+//               >
+//                 <Text style={styles.buttonText}>Get Started</Text>
+//               </TouchableOpacity>
+//             </View>
+//           </View>
+//         </Modal>
+//       );
+//     } else {
+//       return (
+//         <>
+//           <GestureHandlerRootView style={{ flex: 1 }}>
+//             <RootSiblingParent>
 
-              <RootLayoutNav />
+//               <RootLayoutNav />
 
-            </RootSiblingParent>
-          </GestureHandlerRootView>
-        </>
-      );
-    }
-  }
-}
+//             </RootSiblingParent>
+//           </GestureHandlerRootView>
+//         </>
+//       );
+//     }
+//   }
+// }
--- app/index.js
+++ app/index.js
@@ -1,134 +1,70 @@
-import Onboarding from 'react-native-onboarding-swiper';
-import React, { useState } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { styles } from '../components/styles';
-import Onboarding1 from '../assets/First.svg';
-import Onboarding2 from '../assets/Second.svg';
-import Onboarding3 from '../assets/Third.svg';
-import { useRouter } from 'expo-router'
+import { Stack, useRouter } from "expo-router";
+import { React, useState } from "react";
+import * as Font from "expo-font";
+import { View, Text, TouchableOpacity, Modal } from "react-native";
+import { styles } from "../components/styles";
+import Splash1 from "../assets/Logo.svg";
+import Splash2 from "../assets/logo2.svg";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { RootSiblingParent } from "react-native-root-siblings";
+import RootLayoutNav from "./_layout";
+import { getValueFor } from "../components/ExpoStorage";
 
 
-//custom onboarding dots to show which page you're on
-const Circle = ({ isLight, selected }) => {
-  let backgroundColor;
-  if (isLight) {
-    backgroundColor = selected ? 'rgba(254, 45, 1, 1)' : 'rgba(254, 45, 1, 0.3)';
-  } else {
-    backgroundColor = selected ? '#fe2d01' : 'rgba(254, 45, 1, 0.3)';
+export default function RootLayout() {
+    const router = useRouter();
+    const [fontLoaded, setFontLoaded] = useState(false);
+    const [splash, setSplash] = useState(true);
+  
+    async function loadFont() {
+      await Font.loadAsync({
+        "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.otf"),
+        "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.otf"),
+        "Bitter-Regular": require("../assets/fonts/Bitter-Regular.otf"),
+        "Montserrat-SemiBold": require("../assets/fonts/Montserrat-SemiBold.otf"),
+      });
+      setFontLoaded(true);
+    }
+    loadFont();
+  
+    if (!fontLoaded) {
+      return <Splash1 />;
+    } else {
+      if (splash) {
+        return (
+        //   <Modal visible={splash} animationType="fade">
+            <View style={styles.modalContainer}>
+              <Splash2 style={styles.backgroundImage} />
+              <View style={styles.contentContainer}>
+                <TouchableOpacity
+                  style={styles.buttonHomeScreen}
+                  onPress={() => {
+                    const onboarding =  getValueFor("onboarding");
+                if (onboarding === "true") {
+                    router.push("(tabs)");
+                } else {
+                    router.push("onboarding");
+                }
+            }}
+                >
+                  <Text style={styles.buttonText}>Get Started</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+        //   </Modal>
+        );
+      } else {
+        return (
+          <>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+              <RootSiblingParent>
+  
+                <RootLayoutNav />
+  
+              </RootSiblingParent>
+            </GestureHandlerRootView>
+          </>
+        );
+      }
+    }
   }
-  return (
-    <View
-      style={{
-        width: 8,
-        height: 8,
-        borderRadius: 4,
-        marginHorizontal: 3,
-        left: 0,
-        top: 20,
-        backgroundColor,
-      }}
-    />
-  );
-};
-
-//onboarding buttons
-const Next = ({ ...props }) => {
-  return (
-<TouchableOpacity style={styles.nextButton} {...props} >
-  <Text style={styles.nextButtonText}>Next</Text>
-</TouchableOpacity>
-  );
-}
-
-const Done = ({ ...props }) => {
-  return (
-<TouchableOpacity style={styles.nextButton} {...props} >
-  <Text style={styles.nextButtonText}>Done</Text>
-</TouchableOpacity>
-  );
-}
-
-const Skip = ({ ...props }) => {
-  return (
-<TouchableOpacity style={styles.skipButton} {...props} >
-  <Text style={styles.skipButtonText}>Skip</Text>
-</TouchableOpacity>
-  );
-}
-export default function OnboardingScreen(){
- const [completed, setCompleted] = useState(false);
-  const router = useRouter();
-  //navigates to the home screen after onboarding is done/if skipped
-  const handleDone = async () => {
-    //await SecureStore.setItem('onboardingComplete', 'true');
-    router.push("(tabs)");
-    setCompleted(true);
-  }
- 
-
-  return (
-    <Onboarding
-    onDone={handleDone}
-    onSkip={handleDone}
-    showSkip={true}
-    bottomBarHighlight={false}
-    bottomBarHeight={50}
-    DotComponent={Circle}
-    NextButtonComponent={Next}
-    DoneButtonComponent={Done}
-    SkipButtonComponent={Skip}
-
-  pages={[
-    {
-      backgroundColor: '#fcfcfc',
-      image: (
-        <Onboarding1 />
-      ),
-      title: (
-        <Text style={styles.onboarding1Title}>Personalize Your Alerts</Text>
-      ),
-      subtitle: (
-        <>
-        <View style={styles.onboardingContainer}>
-        <Text style={styles.onboarding1Subtitle}>Prefer being notified with vibrations rather than auditory alerts? No problem.</Text>
-        <Text style={styles.onboarding1Subtitle}>Choose and adjust alert types in the alerts page, allowing an easy integration of SafeSteps into your life.</Text>
-        </View>
-      
-        </>
-      ),
-    },
-    {
-      backgroundColor: '#fcfcfc',
-      image:(
-        <Onboarding2 style={styles.slide2}/>
-      ),
-      title: (
-        <Text style={styles.onboarding2Title}>View Intersections and Reports</Text>
-      ),
-      subtitle: (
-        <View style={styles.onboardingContainer}>
-           <Text style={styles.onboarding2Subtitle}>Be able to view nearby intersections and its reports on our home page to stay up-to-date.</Text>
-        </View>
-          ),
-       
-    },
-    {
-      backgroundColor: '#fcfcfc',
-      image: (
-        <Onboarding3 style={styles.wormContainer}/>
-      ),
-      title: (
-        <Text style={styles.onboarding1Title}>Submit Reports</Text>
-      ),
-      subtitle: (
-        <View style={styles.onboardingContainer}>
-            <Text style={styles.onboarding2Subtitle}>Report traffic accidents and road obstacles on the reporting page to help keep other users safe and up-to-date.</Text>
-        </View>
-      ),
-      
-    },
-  ]}
-/>
-  );
-}
-
